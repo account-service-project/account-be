c7d4455891b9f395cf7918d881000fbbc6c7a9e8
Meoyoug <eodyd426@gmail.com>
2024-02-08 22:50:12 +0900

    # [feature/find-account-1]  use_balance_history 모델, 스키마 추가 - domain의 하위폴더에 usebalancehistory 폴더생성 - usebalancehistory/models.py 에 모델 정의 - usebalancehistory/schemas.py 에 스키마 정의 - account/models.py에 iter 정의 - account 컨트롤러에서 클래스위의 라우트 데코레이션 삭제
    closes #28

--- domain/account/repository.py
+++ domain/account/repository.py
@@ -16,3 +16,13 @@ class AccountRepository:
     @staticmethod
     def get_count_account(member_id: int) -> int:
         return db.session.query(Account).filter(Account.member_id == member_id).count()
+
+    @staticmethod
+    def get_all_accounts(member_id: int):
+        return Account.query.filter(Account.member_id == member_id).all()
+
+    @staticmethod
+    def delete_account(member_id:int, account_id: int):
+        delete_to_account = Account.query.filter(Account.member_id == member_id, Account.id == account_id).first_or_404()
+        db.session.delete(delete_to_account)
+        db.session.commit()
